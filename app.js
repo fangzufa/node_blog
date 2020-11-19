26090bf7ee7971f9962238d5c500cd59250de7eb
Fzf <fzf@FzfdeMacBook-Pro.local>
2020-11-19 14:33:02 +0800

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const querystring = require('querystring')
 const handleBlogRouter = require('./src/router/blog')
 const handleUserRouter = require('./src/router/user')
-const { get } = require('./src/server/db/redis')
+const { get, set } = require('./src/server/db/redis')
 const { access } = require('./src/utils/log')
 
 // session 数据
@@ -70,69 +70,88 @@ const serverHandle = (req, res) => {
         req.cookie[key] = val
     })
 
-    // 解析 session
+    // // 解析 session
+    // let needSetCookie = false
+    // let userId = req.cookie.userid
+    // if(userId){
+    //     if(!SESSION_DATA[userId]){
+    //         SESSION_DATA[userId] = {}
+    //     }
+    // }
+    // else{
+    //     needSetCookie = true
+    //     userId = `${Date.now()}_${Math.random()}`
+    //     SESSION_DATA[userId] = {}
+    // }
+    // req.session = SESSION_DATA[userId]
 
+    // 解析 session (使用 redis )
     let needSetCookie = false
     let userId = req.cookie.userid
-    get(userId || '').then(userInfo => {
-        if (!userId) {
-            needSetCookie = true
-            userId = `${Date.now()}_${Math.random()}`
+    if (!userId) {
+        needSetCookie = true
+        userId = `${Date.now()}_${Math.random()}`
+        // 初始化 redis 中的 session 值
+        set(userId, {})
+    }
+    // 获取 session
+    req.sessionId = userId
+    get(req.sessionId).then(sessionData => {
+        if (sessionData == null) {
+            // 初始化 redis 中的 session 值
+            set(req.sessionId, {})
+            req.session = {}
         }
-        req.session = userInfo ? userInfo : {}
-        req.sessionId = userId
+        else {
+            // 设置 session
+            req.session = sessionData
+        }
 
         // 处理 postData
-        getPostData(req).then(postData => {
-            req.body = postData
+        return getPostData(req)
+    }).then(postData => {
+        req.body = postData
 
-            // 处理 blog 路由
-            const blogResulr = handleBlogRouter(req, res)
-            if (blogResulr) {
-                blogResulr.then(blogData => {
-                    if (needSetCookie) {
-                        // 操作 cookie  httpOnly 只允许server端操作修改
-                        // 前端修改不了 会新增一个 同名的cookie
-                        res.setHeader('Set-Cookie', `userid=${userId}; path=/; httpOnly; expires=${getCookieExpires()}`)
-                    }
+        // 处理 blog 路由
+        const blogResulr = handleBlogRouter(req, res)
+        if (blogResulr) {
+            blogResulr.then(blogData => {
+                if (needSetCookie) {
+                    // 操作 cookie  httpOnly 只允许server端操作修改
+                    // 前端修改不了 会新增一个 同名的cookie
+                    res.setHeader('Set-Cookie', `userid=${userId}; path=/; httpOnly; expires=${getCookieExpires()}`)
+                }
 
-                    res.end(
-                        JSON.stringify(blogData)
-                    )
-                })
-                return
-            }
+                res.end(
+                    JSON.stringify(blogData)
+                )
+            })
+            return
+        }
 
-            // 处理 user 路由
-            const userResulr = handleUserRouter(req, res)
-            if (userResulr) {
-                userResulr.then(userData => {
-                    if (needSetCookie) {
-                        // 操作 cookie  httpOnly 只允许server端操作修改
-                        // 前端修改不了 会新增一个 同名的cookie
-                        res.setHeader('Set-Cookie', `userid=${userId}; path=/; httpOnly; expires=${getCookieExpires()}`)
-                    }
+        // 处理 user 路由
+        const userResulr = handleUserRouter(req, res)
+        if (userResulr) {
+            userResulr.then(userData => {
+                if (needSetCookie) {
+                    // 操作 cookie  httpOnly 只允许server端操作修改
+                    // 前端修改不了 会新增一个 同名的cookie
+                    res.setHeader('Set-Cookie', `userid=${userId}; path=/; httpOnly; expires=${getCookieExpires()}`)
+                }
 
-                    res.end(
-                        JSON.stringify(userData)
-                    )
-                })
-                return
-            }
+                res.end(
+                    JSON.stringify(userData)
+                )
+            })
+            return
+        }
 
-            // 未命中路由，返回 404
-            res.writeHead(404, { 'Content-type': 'text/plain' })
-            res.write('404 Not Found\n')
-            res.end()
-        })
+        // 未命中路由，返回 404
+        res.writeHead(404, { 'Content-type': 'text/plain' })
+        res.write('404 Not Found\n')
+        res.end()
     })
-
 
 }
 
 module.exports = serverHandle;
-
-
-// process.env.NODE_ENV
-
-// 6-3
