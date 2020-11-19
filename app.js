b05f9775b185007e340a80a3e1b2ae1862b5c4b3
Fzf <fzf@FzfdeMacBook-Pro.local>
2020-11-19 14:23:22 +0800

    explan app.js

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-var createError = require('http-errors');
+var createError = require('http-errors'); //处理404
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var cookieParser = require('cookie-parser');//处理cookie
+var logger = require('morgan');//处理日志
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+// view engine setup (前端页面展示)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json());//处理POST数据
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
